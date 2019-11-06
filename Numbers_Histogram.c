640fd71b5991199a1c2d2ea90a53c0455c097f9a
InvincibleFarrukh <55508182+InvincibleFarrukh@users.noreply.github.com>
2019-11-06 10:35:45 +0500

    Update Numbers_Histogram.c

--- Numbers_Histogram.c
+++ Numbers_Histogram.c
@@ -3,17 +3,17 @@
 
 int main()
 {
-    int n[SIZE]={19,2,15,7,11,9,13,5,17,1};
+    int array[SIZE]={19,2,15,7,11,9,13,5,17,1};
 
     int i,j;
 
     printf("%s%13s%17s\n", "Element", "Value", "Histogram");
     
-    for(i=0; i<=n[SIZE]-1;i++)
+    for(i=0; i<=array[SIZE]-1;i++)
     {
-        printf("%7d%13d           ", i, n[i]);
+        printf("%7d%13d           ", i, array[i]);
 
-        for (j=1; j<=n[i];j++)
+        for (j=1; j<=array[i];j++)
             printf("%c", '*');
             printf("\n");
     }
